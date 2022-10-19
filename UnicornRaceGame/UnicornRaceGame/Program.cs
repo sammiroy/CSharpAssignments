c8b040a49c093c37d4cfb34107c57d6657fdca11
sammiroy <90281906+sammiroy@users.noreply.github.com>
2022-10-19 14:37:02 -0600

    Updated Unicorn Race

--- UnicornRaceGame/UnicornRaceGame/Program.cs
+++ UnicornRaceGame/UnicornRaceGame/Program.cs
@@ -4,58 +4,101 @@
     {
         static void Main(string[] args)
         {
-            int trackLength = 20;
-            int playerPos = 17;
+            // INIT VARS
+            // Unset Variables
+            string playerInput;
+            int trackLength;
+            
+            Random roll = new Random();
 
-            // Gameplay Loop
-            while (true)
+            // Set Variables
+            int playerPrevRoll = 0, enemyPrevRoll = 0;
+            bool noWinner = true;
+            int playerPos = 0;
+            int enemyPos = 0;
+
+            
+            // Player Assigned Values
+
+            Console.Write("Please enter a track length: ");
+            playerInput = Console.ReadLine();
+
+            // Validate Track Length is >0
+            while (!int.TryParse(playerInput, out trackLength) || trackLength < 1)
             {
-                //DRAW TRACK, NO TOUCHY
-                for (int i = 0; i < trackLength; i++)
-                {
-                    Console.Write("=");
-                }
-                Console.WriteLine();
-                Console.WriteLine();
-                for (int i = 0; i < trackLength; i++)
-                {
-                    if (playerPos == i)
-                    {
-                        Console.Write("*");
-                    }
-                    else
-                    {
-                        Console.Write(" ");
-                    }
-
-                }
-                Console.WriteLine();
-                Console.WriteLine();
-                for (int i = 0; i < trackLength; i++)
-                {
-                    if (playerPos == i)
-                    {
-                        Console.Write("*");
-                    }
-                    else
-                    {
-                        Console.Write(" ");
-                    }
-
-                }
-                Console.WriteLine();
-                Console.WriteLine();
-                for (int i = 0; i < trackLength; i++)
-                {
-                    Console.Write("=");
-                }
+                Console.Clear();
+                Console.WriteLine("Error! You have entered an invalid track length.");
+                Console.Write("Please enter a valid track length (int > 0): ");
+                playerInput = Console.ReadLine();
             }
-            
-        }
+            // Gameplay Loop
+            while (noWinner)
+            {
+                Console.Clear();
+                // Display Stats
+                Console.WriteLine($"You are at position {playerPos+1}, you rolled a {playerPrevRoll} last round");
+                Console.WriteLine($"Your rival is at position {enemyPos + 1}, they rolled a {enemyPrevRoll} last round");
 
-        static void DrawTrack(int trackLength, int playerPos)
-        {
-            
+                // Draw Track 
+                // Top Fence
+                for (int i = 0; i < trackLength; i++)
+                {
+                    Console.Write("=");
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+                // Player
+                for (int i = 0; i < trackLength; i++)
+                {
+                    if (playerPos == i)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+                //  Enemy
+                for (int i = 0; i < trackLength; i++)
+                {
+                    if (enemyPos == i)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+                for (int i = 0; i < trackLength; i++)
+                {
+                    Console.Write("=");
+                }
+                Console.WriteLine();
+                Console.WriteLine("Press Enter to go next roll.");
+                Console.ReadLine();
+
+                // Check if an ending state has been achieved
+                if (playerPos >= trackLength || enemyPos >= trackLength)
+                {
+                    noWinner = false;
+                }
+                
+                // Roll New Positions 
+                playerPrevRoll = roll.Next(1, 7);
+                playerPos += playerPrevRoll;
+
+                enemyPrevRoll = roll.Next(1, 7);
+                enemyPos += enemyPrevRoll;
+
+            }
         }
     }
 }
