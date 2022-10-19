f6927ef3621d23ab5f27c145a32640b7a4726482
sammiroy <90281906+sammiroy@users.noreply.github.com>
2022-10-18 20:59:20 -0600

    Added drawing loop

---
+++ UnicornRaceGame/UnicornRaceGame/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace UnicornRaceGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int trackLength = 20;
+            int playerPos = 17;
+
+            // Gameplay Loop
+            while (true)
+            {
+                //DRAW TRACK, NO TOUCHY
+                for (int i = 0; i < trackLength; i++)
+                {
+                    Console.Write("=");
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+                for (int i = 0; i < trackLength; i++)
+                {
+                    if (playerPos == i)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+                for (int i = 0; i < trackLength; i++)
+                {
+                    if (playerPos == i)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+                for (int i = 0; i < trackLength; i++)
+                {
+                    Console.Write("=");
+                }
+            }
+            
+        }
+
+        static void DrawTrack(int trackLength, int playerPos)
+        {
+            
+        }
+    }
+}
