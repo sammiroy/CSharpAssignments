c61c2c7cff83a82348b24b1753cfe2dd3eeab05f
sammiroy <90281906+sammiroy@users.noreply.github.com>
2022-11-23 17:25:56 -0700

    null

--- Assignment3Part2-SammiRoy/Assignment3Part2-SammiRoy/Program.cs
+++ Assignment3Part2-SammiRoy/Assignment3Part2-SammiRoy/Program.cs
@@ -1,16 +1,18 @@
 ﻿namespace Assignment3Part2_SammiRoy;
+using static System.Console;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        
+        GetMenuChoice();
     }
 
     // Display Menu, process information
     static void DisplayMenu()
     {
-        Console.WriteLine(
+        Clear();
+        WriteLine(
             "CPSC1012 Lotto Centre\n" +
             "-------------------------------------\n" +
             "1. Change Lotto MAX winning numbers\n" +
@@ -22,6 +24,31 @@ internal class Program
     }
 
     /// <summary>
+    /// Check User Input 
+    /// </summary>
+    /// <returns></returns>
+    static int GetMenuChoice()
+    {
+        // Display Menu
+        DisplayMenu();
+
+        // Get input from user
+        string userInput = ReadLine();
+
+        // Validate Input
+        while (userInput != "1" && userInput != "2" && userInput != "3" && userInput != "4" && userInput != "5" && userInput != "0")
+        {
+            WriteLine("Error! Invalid menu selection.");
+            DisplayMenu();
+            userInput = ReadLine();
+        }
+
+        // Return valid input
+        int selection = int.Parse(userInput);
+        return selection;
+    }
+
+    /// <summary>
     /// Prompt user for a string, check if it is not empty
     /// </summary>
     /// <param name="prompt">The message displayed to the user</param>
@@ -30,14 +57,13 @@ internal class Program
     {
         string userInput;
 
-        Console.Write(prompt);
-        userInput = Console.ReadLine();
+        Write(prompt);
+        userInput = ReadLine();
 
         while (string.IsNullOrEmpty(userInput))
         {
-            Console.WriteLine("Error, invalid string!");
-            Console.Write("Please try again: ");
-            userInput = Console.ReadLine();
+            Write("Error! Please try again: ");
+            userInput = ReadLine();
         }
 
         return userInput;
@@ -53,14 +79,13 @@ internal class Program
         string userInput;
         int validOutput;
 
-        Console.Write(prompt);
-        userInput = Console.ReadLine();
+        Write(prompt);
+        userInput = ReadLine();
 
-        while (!int.TryParse(userInput, out validOutput) || validOutput < 0)
+        while (!int.TryParse(userInput, out validOutput) || validOutput <= 0)
         {
-            Console.WriteLine("Error, inavlid integer!");
-            Console.Write("Please try again: ");
-            userInput = Console.ReadLine();
+            Write("Error! Please try again: ");
+            userInput = ReadLine();
         }
 
         return validOutput;
